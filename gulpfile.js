7baaec60c840c20f8327b0f2bd871e5db80d29d8
Arnaud Esteve <arnaud.esteve@gmail.com>
2015-06-28 13:18:07 +0200

    Gulp

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var rename = require('gulp-rename');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+var es = require('event-stream');
+
+
+var requireFiles = './node_modules/react/react.js';
+
+
+function compileScripts(watch) {
+    gutil.log('Starting browserify');
+
+    var files = [
+    	'./src/js/main-page.jsx',
+    	'./src/js/match-page.jsx'
+    ];
+
+    var tasks = files.map(function(entry) {
+       	var startName = entry.lastIndexOf('/') + 1;
+    	var endName = entry.lastIndexOf('-page.jsx');
+    	var newName = entry.substring(startName, endName);
+    
+	    var bundler;
+	    if (watch) {
+	        bundler = watchify;
+	    } else {
+	        bundler = browserify;
+	    }
+	
+        return bundler({ entries: [entry] })
+        	.transform(babelify)
+            .bundle()
+            .pipe(source(entry))
+            .pipe(rename({
+            	dirname:'./',
+            	basename:newName,
+                extname: '.min.js'
+            }))
+            .pipe(gulp.dest('./web/assets/js'));
+    });
+	return es.merge.apply(null, tasks);
+}
+
+
+gulp.task('compile', function () {
+	compileScripts();
+});
